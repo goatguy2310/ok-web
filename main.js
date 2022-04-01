6c9e47b5fc53474e4ecbd66b2ebac8947a4ea742
GoatGuy <goatsaregood05@gmail.com>
2022-04-01 23:21:29 +0700

    added all mechanics

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
+    let word = "";
+    let arr = [];
+    let ord = 0;
+
     createBoard();
     pickWord();
-    let word = "";
+
+    function noTone(str) {
+        str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g,"a"); 
+        str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g,"e"); 
+        str = str.replace(/ì|í|ị|ỉ|ĩ/g,"i"); 
+        str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g,"o"); 
+        str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g,"u"); 
+        str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g,"y"); 
+        str = str.replace(/đ/g,"d");
+        str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+        str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+        str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+        str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+        str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+        str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+        str = str.replace(/Đ/g, "D");
+
+        return str;
+    }
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+      }
 
     function createBoard() {
         const board = document.getElementById("gameboard");
@@ -15,39 +41,81 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function pickWord() {
-        var text = null;
-        var req = new XMLHttpRequest();
+        let req = new XMLHttpRequest();
         req.open("GET", "http://127.0.0.1:5500//words.txt", false);
         req.send();
         if(req.status === 200 || req.status === 0) {
-            text = req.responseText;
+            word = noTone(req.responseText);
+            arr = word.split("\n");
+
+            for(let i = 0; i < arr.length; i++) {
+                arr[i] = arr[i].substring(0, 7);
+            }
+
+            ord = getRandomInt(arr.length);
+            word = arr[ord];
+            console.log(ord);
+            console.log(word);
         }
     }
 
     let cur = 0;
     let curWord = 0;
+    let w = "";
     let btn = document.querySelectorAll(".keyboard-row button");
+    let spaceNum = 0;
+    let done = false;
 
     for(let i = 0; i < btn.length; i++) {
         btn[i].addEventListener("click", () => {
+            if(done) return;
             key = btn[i].getAttribute("data-key");
             if(key == "space") {
-
+                addSpace();
             } else if(key == "del") {
-                deleteLetter(key);
+                deleteLetter();
             } else if(key == "enter") {
-
+                enterWord();
             } else {
                 addLetter(key);
             }
         });
     }
 
+    document.addEventListener("keydown", (event) => {
+        if(done) return;
+        if(event.key === "Enter") {
+            enterWord();
+        } else if(event.key === "Backspace") {
+            deleteLetter();
+        } else if(event.key === " ") {
+            addSpace();
+        } else if((/[a-zA-Z]/).test(event.key) && event.key.length == 1) {
+            addLetter(event.key);
+        }
+    });
+
     function addLetter(letter) {
         if(Math.floor(cur / 7) != curWord) return;
 
         const sq = document.getElementById(String(cur));
         sq.textContent = letter.toUpperCase();
+        w += letter;
+        cur++;
+    }
+
+    function addSpace() {
+        if(Math.floor(cur / 7) != curWord) return;
+
+        if(spaceNum == 1) {
+            alert("1 space only!!!!!!!");
+            return;
+        }
+        spaceNum++;
+
+        const sq = document.getElementById(String(cur));
+        sq.textContent = " ";
+        w += " ";
         cur++;
     }
 
@@ -56,6 +124,45 @@ document.addEventListener("DOMContentLoaded", () => {
 
         cur--;
         const sq = document.getElementById(String(cur));
+        if(sq.textContent == " ") spaceNum--;
+        w = w.substring(0, w.length - 1);
         sq.textContent = "";
+    }
+
+    function enterWord() {
+        if(w.length != 7) {
+            alert("Not 7 letters!!!!!!!");
+            return;
+        }
+
+        if(!arr.includes(w)) {
+            alert("Not a real word!!!!!!");
+            return;
+        }
+
+        if(spaceNum == 0) {
+            alert("Need 1 space!!!!!!");
+            return;
+        }
+
+        for(let i = cur - 7, j = 0; i < cur; i++, j++) {
+            let sq = document.getElementById(String(i));
+            if(sq.textContent.toLowerCase() == word[j]) {
+                sq.style.backgroundColor = "rgb(83,141,78)";
+                sq.style.outlineColor = "rgb(83,141,78)";
+            } else if(word.includes(sq.textContent.toLowerCase())) {
+                sq.style.backgroundColor = "rgb(181,159,59)";
+                sq.style.outlineColor = "rgb(181,159,59)";
+            }
+        }
+
+        if(w == word) {
+            alert("Congrats!!!!!!!!");
+            done = true;
+        }
+
+        curWord++;
+        spaceNum = 0;
+        w = "";
     }
 });
